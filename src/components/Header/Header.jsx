1ff168ec55cbc4acea17fbbaf30ec1b8f867b143
safinahmedhasan <safinahmedhaasan@gmail.com>
2023-03-30 23:23:11 +0600

    first commit

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Header.css'
+
+const Header = () => {
+    return (
+        <div className='header-section'>
+            <h2 className='header-title'>programming Cafe</h2>
+            <img className='header-image' src="https://images.unsplash.com/photo-1615109398623-88346a601842?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80" alt="" />
+        </div>
+    );
+};
+
+export default Header;
