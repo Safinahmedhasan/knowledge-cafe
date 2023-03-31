70e9f62dd7e00a83fba380513bd3515ddc92e6cd
safinahmedhasan <safinahmedhaasan@gmail.com>
2023-03-31 23:46:43 +0600

    bookmark section added

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,7 +4,7 @@ import './Header.css'
 const Header = () => {
     return (
         <div className='header-section'>
-            <h2 className='header-title'>programming Cafe</h2>
+            <h2 className='header-title'>Programming Cafe</h2>
             <img className='header-image' src="https://images.unsplash.com/photo-1615109398623-88346a601842?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80" alt="" />
         </div>
     );
